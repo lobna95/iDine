e854ac14a8c68ffd53801aab5e0dc0e370996ceb
Lobna Shaheen <lobna.mostafa95@gmail.com>
2021-07-18 16:39:15 +0200

    Detailed Items

---
+++ iDine/ItemDetail.swift
@@ -0,0 +1,38 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Lobna Shaheen on 18/07/2021.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    let item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing){
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(Color.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+            }
+            Text(item.description)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetail(item: MenuItem.example)
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Lobna Shaheen on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
